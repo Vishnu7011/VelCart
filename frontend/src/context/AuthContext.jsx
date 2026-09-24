7f43f118c25edbaecf4ecffe94e3e7b5fcb6cf66
Vishnu <vishnupal220@gmail.com>
2026-09-24 11:41:31 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from 'react'
 
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://velcart-backend.onrender.com"
 
     let value = {
         serverUrl
