a9b5c4a4b91b20a6680b73954a11157ea966a6f4
Vishnu <vishnupal220@gmail.com>
2026-09-24 12:27:59 +0530

    Update  index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["https://velcart-frontendone.onrender.com" , "http://localhost:5174"],
+    origin:["https://velcart-frontendone.onrender.com" , "https://velcart-admin.onrender.com"],
     credentials:true
 }))
 
