b2f14979f30b56fb9c42e815b5218608a5deb7d6
Vishnu <vishnupal220@gmail.com>
2026-09-26 15:35:06 +0530

    Update auth

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -62,7 +62,11 @@ export const login = async (req,res) => {
 
 export const logOut = async (req,res) => {
 try {
-    res.clearCookie("token")
+    res.clearCookie("token",{
+        httpOnly:true,
+        secure:true,
+        sameSite:"none"
+    })
     return res.status(200).json({message:"logOut successfull"})
 } catch (error) {
     console.log("logOut error")
