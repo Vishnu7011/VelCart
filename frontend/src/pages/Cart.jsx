516671ad8aa1defb6a97a287d57cbe6ae14cdab3
Vishnu <vishnupal220@gmail.com>
2026-09-26 16:39:49 +0530

    Adjust

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -40,16 +40,16 @@ function Cart() {
 
             return (
                 <div key={index} className='w-[100%] h-[10%] border-t border-b'>
-                  <div className='w-[100%] h-[80%] flex items-start gap-6 bg-[#51808048] py-[10px] px-[20px] rounded-2xl relative'>
+                  <div className='w-[100%] h-[80%] flex items-start gap-6 bg-[#51808048] py-[10px] px-[7px] rounded-2xl relative'>
                     <img className='w-[100px] h-[100px] rounded-md' src={productData.image1} alt=""/>
                      <div className='flex items-start justify-center flex-col gap-[10px]'>
                        <p className='md:text-[25px] text-[20px] text-[#f3f9fc]'>{productData.name}</p>
-                      <div className='flex items-center gap-[20px]'>
-                        <p className='text-[20px] text-[#aaf4e7]'>{currency} {productData.price}</p>
-                        <p className='w-[40px] h-[40px] text-[16px] text-[white] bg-[#518080b4] rounded-md mt-[5px] flex items-center justify-center border-[1px] border-[#9ff9f9]'>{item.size}</p>
+                      <div className='flex items-center gap-[10px]'>
+                        <p className='text-[18px] text-[#aaf4e7]'>{currency} {productData.price}</p>
+                        <p className='w-[40px] h-[40px] text-[18px] text-[white] bg-[#518080b4] rounded-md mt-[5px] flex items-center justify-center border-[1px] border-[#9ff9f9]'>{item.size}</p>
                      </div>
                   </div>
-                  <input type='number' min={1} defaultValue={item.quantity} className='md:max-w-20 max-w-10 md:px-2 md:py-2 py-[5px] px-[10px] text-[white] text-[18px]  font-semibold bg-[#518080b4] absolute md:top-[20%] top-[46%] left-[75%] md:left-[50%] border-[1px] border-[#9ff9f9] rounded-md' 
+                  <input type='number' min={1} defaultValue={item.quantity} className='md:max-w-20 max-w-10 md:px-2 md:py-2 py-[5px] px-[5px] text-[white] text-[18px]  font-semibold bg-[#518080b4] absolute md:top-[20%] top-[46%] left-[75%] md:left-[50%] border-[1px] border-[#9ff9f9] rounded-md' 
                     onChange={(e)=> e.target.value === ' ' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,Number(e.target.value))} />
                     <RiDeleteBin6Line className='text-[#9ff9f9] w-[25px] h-[25px] absolute top-[50%] md:top-[40%] md:right-[5%] right-1' onClick={()=>updateQuantity(item._id,item.size,0)}/>
                   </div>
